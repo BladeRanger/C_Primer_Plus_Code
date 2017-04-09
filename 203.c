f25ec93c35108c05a1cd4b20c884e9d78c7f05e8
Jian Ding <dingjian42@gmail.com>
2017-04-09 21:18:50 +0100

    Excercise 203 204 205 206 207 208

---
+++ 203.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+  int age = 36;
+  printf("I am %d years old, that is %d days.\n", age, age * 365);
+
+  return 0;
+}
---
+++ 206.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+  int toes = 10;
+
+  printf("toes = %d, twice toes = %d, toes squared = %d\n", toes, toes * 2, toes * toes);
+
+  return 0;
+}
