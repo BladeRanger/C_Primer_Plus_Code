c5554c07fddfe5998dc205663ea31dd37dc96cf9
Jian Ding <dingjian42@gmail.com>
2017-04-30 21:26:03 +0100

    303

---
+++ 303.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+  printf("%c", 7);
+  printf("Startled by the sudden sound, Sally shouted,\n");
+  printf("\"By the Great Pumpkin, what was that!\"\n");
+
+  return 0;
+}
